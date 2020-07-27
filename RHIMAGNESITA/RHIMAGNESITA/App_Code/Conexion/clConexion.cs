607460bef6daa8fe90d31d9b71eba410c22e6625
Leon <andresleon080216@gmail.com>
2020-07-26 20:12:08 -0500

    Creacion de clase conexion y su metodo

---
+++ RHIMAGNESITA/RHIMAGNESITA/App_Code/Conexion/clConexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Descripción breve de clConexion
+/// </summary>
+public class clConexion
+{
+    public clConexion()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+        //metodo de conexion
+        public void mtdConexion()
+        {
+
+        }
+
+    }
+}
