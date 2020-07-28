e37ecf1644dc0bf1c546786ba933a40c86403ab6
YeiKinnG <60720049+yeikinng@users.noreply.github.com>
2020-07-28 15:28:28 -0500

    Solucion Registro de comboBox

--- RHIMAGNESITA/RHIMAGNESITA/Usuario.aspx.cs
+++ RHIMAGNESITA/RHIMAGNESITA/Usuario.aspx.cs
@@ -9,15 +9,13 @@ public partial class Usuario :  System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
-        mtdCargarCombo();
-
+      
     }
-    //Lista del Rol
-    List<clRolE> listaRol = new List<clRolE>();
-
-    private void mtdCargarCombo()
+    protected void Page_Init(object sender, EventArgs e)
     {
+        //Lista del Rol
+        List<clRolE> listaRol = new List<clRolE>();
+
         //Clase Rol junto con el metodo listar
         clRolL objRolL = new clRolL();
         listaRol = objRolL.mtdListarRol();
@@ -26,12 +24,10 @@ public partial class Usuario :  System.Web.UI.Page
         cmbRol.DataSource = listaRol;
         cmbRol.DataTextField = "Rol";
         cmbRol.DataValueField = "IdRol";
-
         cmbRol.DataBind();
-        
-        cmbRol.SelectedIndex = 0;
-
     }
+
+
     protected void btnGuardar_Click(object sender, EventArgs e)
     {
         clUsuarioE objUsuarioE = new clUsuarioE();
@@ -43,15 +39,15 @@ public partial class Usuario :  System.Web.UI.Page
         objUsuarioE.Clave = txtClave.Text;
         objUsuarioE.Ciudad = txtCiudad.Text;
         objUsuarioE.Direccion = txtDireccion.Text;
-        objUsuarioE.IdRol = int.Parse(cmbRol.Text);
+        objUsuarioE.IdRol = int.Parse(cmbRol.SelectedValue.ToString());
 
         clUsuario objUsuario = new clUsuario();
         int resultsql = objUsuario.mtdRegistrarUsuario(objUsuarioE);
 
         if (resultsql > 0)
         {
-            mtdCargarCombo();
             //enviar mensaje 
+            //Limpiar Campos de texto
             Response.Write("<script>alert('Se Registro Correctamente');</script>");
             txtDocumento.Text = "";
             txtNombre.Text = "";
