1a38e50b7af08d85c4b3958f10e9f592b68beb93
Leon <andresleon080216@gmail.com>
2020-08-08 20:08:33 -0500

    Creacion de texto usuario en las vistas

--- RHIMAGNESITA/RHIMAGNESITA/Login.aspx.cs
+++ RHIMAGNESITA/RHIMAGNESITA/Login.aspx.cs
@@ -19,7 +19,7 @@ public partial class Login : System.Web.UI.Page
         if (tblDatos.Rows.Count > 0)
         {
             Session["usuario"] = tblDatos.Rows[0][0].ToString();
-            Session["rol"] = tblDatos.Rows[0][2].ToString();
+            Session["rol"] = tblDatos.Rows[0][1].ToString();
             Response.Redirect("~/Inicio1.aspx");
         }
         else
